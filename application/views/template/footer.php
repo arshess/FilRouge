c57dfeb269f5d903cf543bed4641beff2df5a412
AhmedKoga <valentino-koga@hotmail.com>
2021-03-09 16:52:26 +0100

    configuration plus creation footer et header

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,5 +1,17 @@
-</div>
-<script src="<?=base_url()?>public/script.js"></script>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        
+            <div class="navbar-nav container row m-auto">
+                <a class="nav-link col-3" href="#">MENTION LEGALE</a>
+                <a class="nav-link col-3" href="#">PLAN DU SITE</a>
+                <a class="nav-link col-3" href="#">CGU/CGV</a>
+                <a class="nav-link col-3" href="#">POLITIQUE DE COOKIES</a>
+            </div>
+      
+    </div>
+</nav>
+
+<script src="<?= base_url() ?>public/script.js"></script>
 </body>
 
 </html>
