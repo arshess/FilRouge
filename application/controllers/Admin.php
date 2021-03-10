3c5c60143ee54c4f0e88b64552bc2a0feccdda7c
ChristopheG80 <ch.goussard@gmail.com>
2021-03-10 14:00:36 +0100

    create admin location

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,6 +6,30 @@ class Admin extends CI_Controller {
 	
 	public function index()
 	{
-		$this->load->view('welcome_message');
+//		$this->load->view('public/index');
+$this->load->model('admin_model');
+$data = $this->admin_model->showAdminLocationToValidate();
+$this->load->view('views/template/header');
+$this->load->view('views/admin/viewAllLocation', $data);
+$this->load->view('views/template/footer');
 	}
+
+	public function show_location(){
+		$this->load->model('admin_model');
+		$data = $this->admin_model->showAdminLocationToValidate();
+		$this->load->view('views/template/header');
+      $this->load->view('views/admin/viewAllLocation', $data);
+      $this->load->view('views/template/footer');
+	}
+
+
+
+
+
+
+
+
 }
+
+
+
