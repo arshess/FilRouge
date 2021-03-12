c31a9de37325b3ce37d97a7b9f550a6f1ac70f2e
ChristopheG80 <ch.goussard@gmail.com>
2021-03-12 16:52:07 +0100

    create admin vehicle

--- application/views/admin/showAdminLocation.php
+++ application/views/admin/showAdminLocation.php
@@ -1,3 +1,4 @@
 <div>
-<a href= "<?= base_url() ?>Admin/showAdminLocation" class="btn btn-secondary">Enregistrer Locations</a><a href= "<?= base_url() ?>Admin/" class="btn btn-secondary">Enregistrer Retours</a>
+<a href="<?= base_url() ?>Admin/showAdminLocation" class="btn btn-secondary">Enregistrer Locations</a>
+<a href="<?= base_url() ?>Admin/showAdminReturn" class="btn btn-secondary">Enregistrer Retours</a>
 </div>
